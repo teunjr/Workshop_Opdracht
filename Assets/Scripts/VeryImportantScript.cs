5d90a064d785883cee568330cefe847bc0d21efb
luuky50 <luukhulshof@hotmail.nl>
2021-03-16 16:17:59 +0100

    VeryImportantScript added

---
+++ Assets/Scripts/VeryImportantScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeryImportantScript : MonoBehaviour
+{
+
+    int carsThatNeedToCrash = 10;
+
+    void Start()
+    {
+        TeslaNavigationSystem();
+    }
+
+    private void TeslaNavigationSystem()
+    {
+        for (int i = 0; i < carsThatNeedToCrash; i++)
+        {
+            Debug.Log("Very Important stuff happening");
+            i++;
+        }
+
+    }
+}
